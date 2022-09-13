70c1698925c7e18d1256fd2063ae0b672d03014f
Anna <annamosolowa@gmail.com>
2022-09-13 21:57:29 +1000

    задача №2

---
+++ Test1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Ввведите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Ввведите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+ 
+if (a > b){
+    Console.Write("max=");
+    Console.WriteLine(a);
+}
+else {
+    Console.Write("max=");
+    System.Console.WriteLine(b);
+}
