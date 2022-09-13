849a7405f413bdbc9e34138681465803cfccadd2
Anna <annamosolowa@gmail.com>
2022-09-13 23:17:35 +1000

    Задача №8

---
+++ Test4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число a: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int a = 1;
+
+while (a <= n-1){
+    System.Console.WriteLine();
+    a++;
+    if (a % 2 == 0) Console.WriteLine(a);
+}
