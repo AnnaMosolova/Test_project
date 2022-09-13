e32b8bbcf976f54030483e746fbb8adab14d777f
Anna <annamosolowa@gmail.com>
2022-09-13 22:59:46 +1000

    задача № 6

---
+++ Test3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0) {
+    Console.WriteLine("да");
+}
+else {
+    System.Console.WriteLine("нет");
+}
